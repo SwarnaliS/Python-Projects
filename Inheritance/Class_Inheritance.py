8e4dae76f23b7d62eb0f56d3ce2bee94b2b13461
SwarnaliS <90927960+SwarnaliS@users.noreply.github.com>
2021-11-18 09:40:27 -0700

    Adding Inheritance assignment

---
+++ Inheritance/Class_Inheritance.py
@@ -0,0 +1,16 @@
+#Declaring vehicle class as parent class
+class vehicle:
+    Type = ''
+    Make = ''
+    Model = ''
+    Color = ''
+# Declaring car as a child class with two new attributes
+class car(vehicle):
+    Engine_Type = ''
+    Max_Speed = ''
+# Declaring truck as a child class with two new attributes
+class truck(vehicle):
+    Weight_limit = ''
+    Turning_Requirents = ''
+    
+    
